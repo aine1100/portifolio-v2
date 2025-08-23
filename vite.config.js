091bf86b11f9e1330956e0ce3fe11560c39d1a53
aine1100 <149615514+aine1100@users.noreply.github.com>
2025-08-23 20:56:33 +0200

    ++

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import compression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression({
-      algorithm: 'gzip',
-      ext: '.gz'
-    })
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
